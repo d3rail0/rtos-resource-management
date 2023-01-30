eef88e0a4a3af0c3c43551cec2df3cf34606216e
Eldin <eldinhelja@live.com>
2023-01-30 01:26:23 +0100

    Added functionality to suspend resource threads when temperature goes out of specified bounds

--- Project/TemperatureControl.cpp
+++ Project/TemperatureControl.cpp
@@ -5,6 +5,10 @@ const uint8_t MQ_BUFFER_SIZE = 8;
 const float MAX_VOLTAGE      = 5.0f;
 const float ADC_RESOLUTION   = 4096.f; 
 
+// Minimal and maximal temperatures allowed for the system
+const float MIN_TEMP = 12.0f;
+const float MAX_TEMP = 25.0f;
+
 osMessageQueueId_t msgQueue_id;
 osThreadAttr_t tempTaskAttrs  = {};
 bool isResourceTasksSuspended = false;
@@ -31,6 +35,10 @@ void TempControllerMain() {
 }
 
 void stopAllResourceTasks() {
+    if (isResourceTasksSuspended)
+        // Already suspended.
+        return;
+
     for(size_t i = sizeof(resourceThreads)/sizeof(osThreadId_t); i > 0; --i)
         osThreadSuspend(resourceThreads[i-1]);
 
@@ -38,29 +46,44 @@ void stopAllResourceTasks() {
 }
 
 void resumeAllResourcesTasks() {
-for(size_t i = sizeof(resourceThreads)/sizeof(osThreadId_t); i > 0; --i)
+    if (!isResourceTasksSuspended)
+        // Tasks were not suspended.
+        return;
+
+    for(size_t i = sizeof(resourceThreads)/sizeof(osThreadId_t); i > 0; --i)
         osThreadResume(resourceThreads[i-1]);
 
     isResourceTasksSuspended = false;
 }
 
+uint32_t getConvertedADCValue() {
+    static uint32_t adcReading { 0 };
+
+    HAL_ADC_Start(&hadc1); // Start conversion
+
+    // Wait for end of the conversion and get value
+    if (HAL_ADC_PollForConversion(&hadc1, 250) == HAL_OK) {
+        adcReading = HAL_ADC_GetValue(&hadc1);
+    } else {
+        signalForError(E_TEMP_NOT_READ_ON_TIME);
+    }
+    
+    HAL_ADC_Stop(&hadc1); // Stop conversion
+
+    return adcReading;
+}
+
 void tempReaderTask(void*) {
 
     uint32_t bufferedReading[MQ_BUFFER_SIZE];
-    uint16_t currElement = 0;
-    uint32_t adcReading = 0;
+    uint16_t currElement { 0 };
+    
+    // Call to avoid wrong reading at the start of simulation.
+    osDelay(1500);
 
     for(;;) {
-        HAL_ADC_Start(&hadc1);
-
-        if (HAL_ADC_PollForConversion(&hadc1, 250) == HAL_OK) {
-            adcReading = HAL_ADC_GetValue(&hadc1);
-            bufferedReading[currElement] = adcReading;
-        } else {
-            signalForError(E_TEMP_NOT_READ_ON_TIME);
-        }
         
-        HAL_ADC_Stop(&hadc1);
+        bufferedReading[currElement] = getConvertedADCValue();
 
         // Push the reading to a queue
         if(osMessageQueuePut(msgQueue_id, &bufferedReading[currElement], 0U, 0U) != osOK) {
@@ -74,6 +97,19 @@ void tempReaderTask(void*) {
 
 }
 
+uint64_t GetTick(void) {
+    static uint32_t tick_h {0U};
+    static uint32_t tick_l {0U};
+    uint32_t tick {osKernelGetSysTimerCount()};
+
+    if (tick < tick_l) {
+        tick_h++;
+    }
+
+    tick_l = tick;
+    return ((static_cast<uint64_t>(tick_h) << 32) | tick_l);
+}
+
 void tempProcessorTask(void*) {
     char     readingMessage[32];
     float    temp;
@@ -83,10 +119,21 @@ void tempProcessorTask(void*) {
     // would end up increasing the FLASH memory size and overflowing, 
     // printing float will require a different approach
     char    tempSign[2] = " "; // can be ' ' or '-'
-    int32_t tempInt     = 0;
-    int32_t fractInt    = 0;
-    float   tempFrac    = 0.f;
-    float   tmpTemp     = 0.f;
+    int32_t tempInt     { 0 };
+    int32_t fractInt    { 0 };
+    float   tempFrac    { 0.f };
+    float   tmpTemp     { 0.f };
+
+    // Wait for producer to send at least a single reading
+    // before starting the main consumer loop.
+    // Note: This will dismiss the first reading completely
+    // which in practice shouldn't cause problems.
+    osMessageQueueGet(msgQueue_id, &adcReading, NULL, osWaitForever);
+
+    uint64_t tick {0};
+  
+    // Calculating 3s timeout in system timer ticks    
+    const uint64_t timeout { 3U * osKernelGetSysTimerFreq() };
 
     for(;;) {
         if(osMessageQueueGet(msgQueue_id, &adcReading, NULL, 250U)) {
@@ -108,6 +155,29 @@ void tempProcessorTask(void*) {
         if(HAL_UART_Transmit(&huart1, reinterpret_cast<uint8_t*>(readingMessage), static_cast<uint16_t>(std::strlen(readingMessage)), 50U) != HAL_OK) {
             signalForError(E_WRITE_FAILED);
         }
+
+        if(temp < MIN_TEMP) {
+            signalForError(E_TEMP_TOO_LOW);
+            tick = GetTick();
+            stopAllResourceTasks();
+        } else if (temp > MAX_TEMP) {
+            signalForError(E_TEMP_TOO_HIGH);
+            tick = GetTick();
+            stopAllResourceTasks();
+        } else if (isResourceTasksSuspended) {
+            
+            // Wait for 
+            if(GetTick() - tick > timeout) {
+                sprintf(readingMessage, "Resuming all threads...\r\n");
+
+                if(HAL_UART_Transmit(&huart1, reinterpret_cast<uint8_t*>(readingMessage), static_cast<uint16_t>(std::strlen(readingMessage)), 250U) != HAL_OK) {
+                    signalForError(E_WRITE_FAILED);
+                }
+
+                resumeAllResourcesTasks();
+            }
+
+        }    
 
         osDelay(250);
     }
